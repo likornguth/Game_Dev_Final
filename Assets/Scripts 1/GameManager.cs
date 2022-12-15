f484f7087a4680f9901438e8698bfb30a014a7ee
Lindsay Kornguth <lindsaykornguth@dyn-209-2-219-207.dyn.columbia.edu>
2022-12-14 22:09:15 -0500

    added pong

---
+++ Assets/Scripts 1/GameManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Ball ball;
+    public AudioSource audioSource;
+    public Paddle playerPaddle;
+    public int playerScore { get; private set; }
+    public Text playerScoreText;
+
+    public Paddle computerPaddle;
+    public int computerScore { get; private set; }
+    public Text computerScoreText;
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            NewGame();
+        }
+    }
+
+    public void NewGame()
+    {
+        SetPlayerScore(0);
+        SetComputerScore(0);
+        StartRound();
+    }
+
+    public void StartRound()
+    {
+        playerPaddle.ResetPosition();
+        computerPaddle.ResetPosition();
+        ball.ResetPosition();
+        ball.AddStartingForce();
+    }
+
+    public void PlayerScores()
+    {
+        audioSource.Play();
+        SetPlayerScore(playerScore + 1);
+        StartRound();
+        
+    }
+
+    public void ComputerScores()
+    {
+        audioSource.Play();
+        SetComputerScore(computerScore + 1);
+        StartRound();
+    }
+
+    private void SetPlayerScore(int score)
+    {
+        playerScore = score;
+        playerScoreText.text = score.ToString();
+    }
+
+    private void SetComputerScore(int score)
+    {
+        computerScore = score;
+        computerScoreText.text = score.ToString();
+    }
+
+}
